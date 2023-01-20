e2d6d47dc9d434b0742e9092c67f3bd23bfdaddf
Tejora <numan.992@gmail.com>
2023-01-20 21:43:37 +0530

    Task commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task21_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<string> lst= new List<string>();
+            lst.Add("Rose");
+            lst.Add("Jasmine");
+            lst.Add("Tulips");
+            lst.Add("Sunflowers");
+            lst.Add("Lotus");
+            lst.Add("Lily");
+            ViewBag.Flowerlst = lst;
+            return View();
+        }
+
+        public ActionResult GetText(string rdFlower)
+        {
+            ViewBag.Output = "Your favourite flower is: " + rdFlower;
+            List<string> lst = new List<string>();
+            lst.Add("Rose");
+            lst.Add("Jasmine");
+            lst.Add("Tulips");
+            lst.Add("Sunflowers");
+            lst.Add("Lotus");
+            lst.Add("Lily");
+            ViewBag.Flowerlst = lst;
+            return View("Index");
+        }
+
+    }
+}
